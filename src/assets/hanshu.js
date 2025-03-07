32949e7eb2f9cf0b3d619a9795dddd70d793dc84
chaoyue <2779107880@qq.com>
2025-03-07 22:40:26 +0800

    Add README file

---
+++ src/assets/hanshu.js
@@ -0,0 +1,18 @@
+function showAlert()
+{
+  const alertDiv = document.getElementById('custom-alert');
+  alertDiv.style.visibility = 'visible';  // 显示弹窗
+}
+
+function closeAlert()
+{
+  const alertDiv = document.getElementById('custom-alert');
+  alertDiv.style.visibility = 'hidden';   // 关闭弹窗
+}
+
+function changeMouse()
+{
+  const changeDiv=document.getElementById('nailong');
+  changeDiv.style.cursor='pointer';
+}
+
